18f0d03dc29767ec0a50069564464f26bb45f895
CMinda <carlosminda0@gmail.com>
2014-07-28 12:54:53 -0500

    LDAP
    OpenLDAP configuration script with a base database file to initiate the
    AD.

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ ldap-setup
 ==========
 
 PCI compliant OpenLDAP setup scripts
+
+This is a script to configure an OpenLDAP Server on a minimal version of Centos 6.5 32 bits.
+Take into account that some values should be edited to your own like the ipaddress,password,admin and Manager.
+
+This setup includes:
+Initial Configuration of Openldap
+Adding Sudo Schema
+Adding Auditlog
+Adding Password Policies
+It also contains a database.ldif file to configure an initial database example that include this parameters and some test users.
+
---
+++ initialconfig.sh
@@ -0,0 +1,111 @@
+setenforce 1
+setsebool -P domain_kernel_load_modules 1
+
+cat >> /etc/hosts << EOF
+10.0.1.33 ldap.test.com
+EOF
+
+# Create folder to store log files in
+mkdir /var/log/slapd
+chmod 755 /var/log/slapd/
+chown ldap:ldap /var/log/slapd/
+
+# Redirect all log files through rsyslog.
+sed -i "/local4.*/d" /etc/rsyslog.conf
+cat >> /etc/rsyslog.conf << EOF
+local4.*                        /var/log/slapd/slapd.log
+EOF
+service rsyslog restart
+
+#tested
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 389 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 636 -j ACCEPT
+
+yum -y install openldap-servers migrationtools
+
+openssl req -new -x509 -nodes -out /etc/pki/tls/certs/centos.cert -keyout /etc/pki/tls/certs/centos.key -days 365
+
+chown -R root:ldap /etc/pki/tls/certs/centos.*
+
+sed -i -e 's/my-domain/test/g' /etc/openldap/slapd.d/cn\=config/olcDatabase\=\{2\}bdb.ldif
+
+sed -i -e 's/my-domain/test/g' /etc/openldap/slapd.d/cn\=config/olcDatabase\=\{1\}monitor.ldif
+
+sed -i -e 's/manager/Manager/g' /etc/openldap/slapd.d/cn\=config/olcDatabase\=\{1\}monitor.ldif
+
+echo "olcRootPW: `slappasswd -s password`" >> /etc/openldap/slapd.d/cn\=config/olcDatabase\=\{2\}bdb.ldif
+
+sed -i -e 's/olcRootDN: cn=config/olcRootDN: cn=admin,cn=config/g' /etc/openldap/slapd.d/cn\=config/olcDatabase\=\{0\}config.ldif
+
+echo "olcRootPW: `slappasswd -s password`" >> /etc/openldap/slapd.d/cn\=config/olcDatabase\=\{0\}config.ldif
+
+echo "olcTLSCertificateFile: /etc/pki/tls/certs/centos.cert" >> /etc/openldap/slapd.d/cn\=config/olcDatabase\=\{2\}bdb.ldif
+
+echo "olcTLSCertificateKeyFile: /etc/pki/tls/certs/centos.key" >> /etc/openldap/slapd.d/cn\=config/olcDatabase\=\{2\}bdb.ldif
+
+cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
+
+sed -i -e 's/LDAPS=no/LDAPS=yes/g' /etc/sysconfig/ldap
+
+chkconfig slapd on
+
+service slapd start
+
+/bin/cp -f  /usr/share/doc/sudo-1.8.6p3/schema.OpenLDAP /etc/openldap/schema/sudo.schema
+restorecon /etc/openldap/schema/sudo.schema
+
+ldapadd -H ldap:/// -x -D "cn=admin,cn=config" -W << EOF
+replace: olcAccess
+olcAccess: {0}to attrs=employeeType by dn="cn=sssd,dc=test,dc=com" read by self read by * none
+olcAccess: {1}to attrs=userPassword,shadowLastChange by self write by anonymous auth by * none
+olcAccess: {2}to dn.base="" by * none
+olcAccess: {3}to * by dn="cn=admin,cn=config" write by dn="cn=sssd,dc=test,dc=com" read by self write by * none
+EOF
+
+
+# Create a conversion file for schema
+mkdir ~/sudoWork
+echo "include /etc/openldap/schema/sudo.schema" > ~/sudoWork/sudoSchema.conf
+# Convert the "Schema" to "LDIF".
+slapcat -f ~/sudoWork/sudoSchema.conf -F /tmp/ -n0 -s "cn={0}sudo,cn=schema,cn=config" > ~/sudoWork/sudo.ldif
+# Remove invalid data.
+sed -i "s/{0}sudo/sudo/g" ~/sudoWork/sudo.ldif
+# Remove last 8 (invalid) lines.
+head -n-8 ~/sudoWork/sudo.ldif > ~/sudoWork/sudo2.ldif
+# Load the schema into the LDAP server
+ldapadd -H ldap:/// -x -D "cn=admin,cn=config" -W -f ~/sudoWork/sudo2.ldif
+ldapadd -H ldap:/// -x -D "cn=admin,cn=config" -W << EOF
+dn: cn=module{0},cn=config
+objectClass: olcModuleList
+cn: module{0}
+olcModulePath: /usr/lib/openldap/
+EOF
+ldapadd -H ldap:/// -x -D "cn=admin,cn=config" -W << EOF
+dn: cn=module{0},cn=config
+changetype:modify
+add: olcModuleLoad
+olcModuleLoad: auditlog.la
+
+dn: olcOverlay=auditlog,olcDatabase={2}bdb,cn=config
+changetype: add
+objectClass: olcOverlayConfig
+objectClass: olcAuditLogConfig
+olcOverlay: auditlog
+olcAuditlogFile: /var/log/slapd/auditlog.log
+EOF
+ldapadd -H ldap:/// -x -D "cn=admin,cn=config" -W << EOF
+dn: cn=module{0},cn=config
+changetype:modify
+add: olcModuleLoad
+olcModuleLoad: ppolicy.la
+
+dn: olcOverlay=ppolicy,olcDatabase={2}bdb,cn=config
+olcOverlay: ppolicy
+objectClass: olcOverlayConfig
+objectClass: olcPPolicyConfig
+olcPPolicyHashCleartext: TRUE
+olcPPolicyUseLockout: TRUE
+olcPPolicyDefault: cn=default,ou=pwpolicies,dc=test,dc=com
+EOF
+
+ldapadd  -H ldap:/// -x -D "cn=Manager,dc=test,dc=com" -W -f database.ldif
